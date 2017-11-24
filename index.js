db9c5fbb47ed93aa5730b7c4e5a69977a64839ba
GabrielAvilesR <gabrielaviles1995@gmail.com>
2017-11-23 19:56:22 -0600

    el hermoso despertador de todas las codornices

---
+++ index.js
@@ -0,0 +1,27 @@
+const http = require('http')
+const port = process.env.PORT || 9000
+const ip = process.env.IP || '0.0.0.0'
+const express = require('express')
+const schedule= require('node-schedule')
+const axios = require('axios')
+
+const app = express()
+const server = http.createServer(app)
+
+//DESPERTANDO A KINTOS SHINGAO
+schedule.scheduleJob('0 */1 * * * *', () => {
+    axios.get(`http://kintos-api.herokuapp.com/`)
+        .then((res) => {
+            console.log('despertando a kintos')
+        })
+        .catch((err) => {
+            console.log('despertando a kintos')
+        })
+})
+
+
+setImmediate(() => {
+  server.listen(port, ip, () => {
+    console.log('Express server listening on http://%s:%d', ip, port)
+  })
+})
